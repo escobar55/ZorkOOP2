4a2cd4daed9bea0854a5b8deada8410a706a1454
escobar55 <escobar5@umbc.edu>
2019-11-13 11:11:00 -0500

    with methods

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,82 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
+        int option = 0;
+        //Room roomOne = new Room("the foyer room #1","a dead scorpion");
+        //Room roomTwo = new Room("the front room #2", "a piano");
+        Room roomThree = new Room("the library room #3", "spiders");
+        Room roomFour = new Room("the kitchen room #4", "bats");
+        Room roomFive = new Room("the dining room #5", "some dust and an empty box");
+        Room roomSix = new Room("the vault room #6", "3 walking skeletons");
+        Room roomSeven = new Room("the parlor room #7", "a treasure chest");
+        Room roomEight = new Room("the secret room #8", "piles of gold");
+        roomOne();
+        if(option == 2){
+            option = roomTwo();
+        }
+    }
 
-        Room roomOne = new Room("the foyer room","a dead scorpion");
-        Room roomTwo = new Room("the front room", "a piano");
-        Room roomThree = new Room("the library room", "spiders");
-        Room roomFour = new Room("the kitchen room", "bats");
-        Room roomFive = new Room("the dining room", "some dust and an empty box");
-        Room roomSix = new Room("the vault room", "3 walking skeletons");
-        Room roomSeven = new Room("the parlor room", "a treasure chest");
-        Room roomEight = new Room("the secret room", "piles of gold");
+    public static int roomOne(){
+        Scanner key = new Scanner(System.in);
+        Room roomOne = new Room("the foyer room #1","a dead scorpion");
+        roomOne.message();
+        int option = 0;
+        System.out.println("You can go North (N), or Quit (Q).");
+        String answer = key.nextLine();
+        if (answer.equalsIgnoreCase("n")) {
+            option = 2;  // will call method roomTwo from inside main()
+        } else if (answer.equalsIgnoreCase("q")) {
+           option =  0;
+        } else {
+            System.out.println("Please enter a valid choice (N:North, Q:Quit): ");
+        }
 
-        //Welcome room
+        return option;
+    }
+
+    public static int roomTwo(){
+        Room roomTwo = new Room("the front room #2", "a piano");
+        roomTwo.message();
+        int option = 0;
+        Scanner key = new Scanner(System.in);
+        System.out.println("You can go South (S), West (W), East (E), or Quit (Q).");
+        String answer = key.nextLine();
+        if (answer.equalsIgnoreCase("s")) {
+            option = 1;  // will call method roomOne from inside main()
+        } else if(answer.equalsIgnoreCase("w")){
+            option = 3; // will call method roomThree from inside main()
+        } else if(answer.equalsIgnoreCase("e")){
+            option =  4; // will call method roomFour from inside main()
+        } else if (answer.equalsIgnoreCase("q")) {
+            option=  0;
+        } else {
+            System.out.println("Please enter a valid choice (S:South, W:West, E:East, Q:Quit): ");
+        }
+        return option;
+    }
+
+}
+
+/*
+
+/Welcome room
         roomOne.message();
         System.out.println("You can go North (N), or Quit (Q).");
         String choice = key.nextLine();
 
-       while(true) {
+     //end while
+
+
+
+
+while(true) {
            if (choice.equalsIgnoreCase("n")) {
                roomTwo.message();
                System.out.println("You can go South (S), West (W), East (E), or Quit (Q).");
                choice = key.nextLine();
+               if(choice.equalsIgnoreCase("s")){
+                   roomOne.message();
+               }
            }
 
            if (choice.equalsIgnoreCase("s")) {
@@ -41,13 +97,15 @@ public class Main {
                roomFour.message();
                System.out.println("You can go West (W), North (N), or Quit (Q).");
                choice = key.nextLine();
+           }
 
-           } else if (choice.equalsIgnoreCase("q")) {
+
+
+           else if (choice.equalsIgnoreCase("q")) {
                System.out.println("Ok, Goodbye!");
                break;
            }
 
-       }//end while
+       }
 
-    }
-}
+ */
